c5cabed855bc1b7d4e611b5ea433a1bb70759f26
RyumenGrok <ayushmi1144@gmail.com>
2026-03-11 19:27:24 +0530

    Update server.js

--- server.js
+++ server.js
@@ -98,6 +98,8 @@ io.on('connection', (socket) => {
     });
 });
 
-server.listen(3000, () => {
-    console.log('Private Hub is running on http://localhost:3000');
+const PORT = process.env.PORT || 10000;
+
+server.listen(PORT, '0.0.0.0', () => {
+    console.log(`Taika is live on port ${PORT}`);
 });
